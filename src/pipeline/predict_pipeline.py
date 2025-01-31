3441e9205dbca7deba0f21a174d6483a3b63dd2f
Vipina7 <vipina1394@gmail.com>
2025-01-31 22:20:14 +0530

    predict pipeline and app.py successfully set up

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -16,26 +16,25 @@ class PredictPipeline:
 
     def predict(self, features):
         try:
-            model_path = 'artifacts/gru_model.h5'
-            vectorizer_path = 'artifacts/fasttest.pkl'
+            model_path = 'artifacts/GRU_model.h5'
+            vectorizer_path = 'artifacts/fasttext.pkl'
 
             model = load_model(model_path)
             logging.info("Successfully installed the model")
 
-            fasttext_url = 'https://drive.google.com/uc?id=1jKlaaiD2EG-t66x584NY0S16UI3jzVWK'
+            fasttext_url = 'https://drive.google.com/uc?id=1wEDqfX623zGUZkaDcLFNwh367DXmJTTm'
             if not os.path.exists(vectorizer_path):
                 download_model_from_drive(url=fasttext_url, output_path=vectorizer_path)
             else:
-                fasttest_model = load_object(file_path=vectorizer_path)
+                fasttext_model = load_object(file_path=vectorizer_path)
 
             features = preprocess_text(features)
-            features = features.split()
-            features = np.array([vectorize_text(word, fasttest_model, embedding_dim=300) for word in features])
-            features = pad_sequences(features, maxlen = 50, padding = 'post', truncating = 'post', dtype = 'float32')
+            features = vectorize_text(features, fasttext_model, embedding_dim=400)
+            features = features.reshape(1,1,400)
 
             prediction = model.predict(features)[0][0] * 100
 
-            return prediction
+            return round(prediction,2)
         
         except Exception as e:
             raise CustomException(e, sys)
